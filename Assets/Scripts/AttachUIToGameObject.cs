eca6fbb19264c094b3399691d8082c4e1b85d113
Daniel Quarrell <Daniel2.Quarrell@live.uwe.ac.uk>
2019-10-23 15:51:07 +0100

    Progress Bar moves to on-screen gameobjects

---
+++ Assets/Scripts/AttachUIToGameObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AttachUIToGameObject : MonoBehaviour
+{
+    [SerializeField] GameObject targetObject;
+
+    private void LateUpdate()
+    {
+        Vector3 UIposition = Camera.main.WorldToScreenPoint(targetObject.transform.position);
+        this.transform.position = UIposition;
+    }
+}
