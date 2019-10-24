cca0cf55d5c6bd1bca816f714522c128ae8acc91
Ellie Dunstan <ellie2.dunstan@live.uwe.ac.uk>
2019-10-24 14:27:35 +0100

    Employees go to a chair after 5 seconds #11

--- Assets/Scripts/Employee.cs
+++ Assets/Scripts/Employee.cs
@@ -6,9 +6,15 @@ using UnityEditor;
 
 public class Employee : MonoBehaviour
 {
+    public enum Gender
+    {
+        MALE, FEMALE
+    }
+    public Gender gender;
+
     public enum State
     {
-        IDLE, MOVING, WORKING
+        IDLE, RELAXING, MOVING, WORKING
     }
     private State state = State.IDLE;
 
@@ -17,8 +23,16 @@ public class Employee : MonoBehaviour
     private NavMeshPath path;
 
     private Animator anim;
+    private float moveSpeed = 0;
+    private float currentMaxSpeed = 1.0f;
+    private float defaultMaxSpeed;
+    private float timeIdle = 0;
 
     public bool debugVisualisation = true;
+
+    private bool shouldRelaxAfterMoving = false;
+    private InteractableFurniture.Interactable currentInteractable = null;
+    private bool atInteractable = false;
 
     public bool Selected
     {
@@ -29,60 +43,183 @@ public class Employee : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
         if (agent == null) Debug.LogError("All employees must have a NavMeshAgent!");
+        defaultMaxSpeed = agent.speed;
 
         anim = GetComponentInChildren<Animator>();
+
+        if (gender == Gender.MALE)
+            anim.SetBool("Male", true);
+        else
+            anim.SetBool("Male", false);
     }
 
     private void Update()
     {
-        float moveSpeed = agent.velocity.magnitude / agent.speed;
-        Debug.Log(moveSpeed);
-        anim.SetFloat("MoveSpeed", moveSpeed);
+        Debug.Log(state);
 
         switch (state)
         {
             case State.IDLE:
-                if (moveSpeed > 0.05)
-                {
-                    state = State.MOVING;
-                }
-                anim.SetBool("Pant", false);
+                UpdateIdle();
+                break;
+            case State.RELAXING:
+                UpdateRelaxing();
                 break;
             case State.MOVING:
-                if (moveSpeed > 0.7f)
-                {
-                    anim.SetBool("Pant", true);
-                }
-
-                if (moveSpeed < 0.05)
-                {
-                    state = State.IDLE;
-                    //if (anim.GetBool("Pant")) StartCoroutine(Panting());
-                }
-
-                if (debugVisualisation)
-                {
-                    for (int i = 0; i < path.corners.Length - 1; i++)
-                    {
-                        Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red);
-                    }
-                }
-
+                UpdateMoving();
                 break;
             case State.WORKING:
                 break;
         }
     }
 
+    private void ChangeState(State newState)
+    {
+        switch (newState)
+        {
+            case State.IDLE:
+                if (currentInteractable != null || atInteractable)
+                {
+                    atInteractable = false;
+                    currentInteractable.occupied = false;
+                    if (currentInteractable.type == InteractableFurniture.Interactable.Type.CHAIR)
+                        anim.SetTrigger("Stand");
+
+                    currentInteractable = null;
+                }
+                break;
+            case State.RELAXING:
+
+                Vector3 rot = new Vector3(
+                    transform.eulerAngles.x,
+                    currentInteractable.origin.eulerAngles.y,
+                    transform.eulerAngles.z);
+                //transform.eulerAngles = rot;
+                transform.rotation = currentInteractable.origin.rotation;
+                if (currentInteractable.type == InteractableFurniture.Interactable.Type.CHAIR)
+                    anim.SetTrigger("Sit");
+
+                break;
+            case State.MOVING:
+
+                break;
+            case State.WORKING:
+
+                break;
+        }
+        state = newState;
+    }
+
+    #region UPDATE STATES
+
+    private void UpdateIdle()
+    {
+        if (moveSpeed > 0.05)
+        {
+            ChangeState(State.MOVING);
+        }
+        if (anim.GetBool("Pant")) anim.SetBool("Pant", false);
+
+        timeIdle += Time.deltaTime;
+        if (timeIdle > 5f)
+        {
+            shouldRelaxAfterMoving = true;
+
+            currentInteractable = InteractableFurniture.Instance.GetInteractable();
+            if (currentInteractable != null)
+                MoveTo(currentInteractable.origin.position);
+            timeIdle = 0;
+        }
+    }
+
+    private void UpdateRelaxing()
+    {
+        if (currentInteractable == null || !atInteractable) return;
+
+        timeIdle += Time.deltaTime;
+        if (timeIdle > 5f)
+        {
+            //find new
+            anim.SetTrigger("Stand");
+        }
+    }
+
+    private void UpdateMoving()
+    {
+        if (CanMove())
+        {
+            currentMaxSpeed = defaultMaxSpeed * (shouldRelaxAfterMoving ? 0.2f : 1.0f);
+            agent.speed = currentMaxSpeed;
+        }
+        else
+        {
+            agent.speed = 0;
+        }
+
+        moveSpeed = agent.velocity.magnitude / defaultMaxSpeed;
+        anim.SetFloat("MoveSpeed", moveSpeed);
+
+
+        if (moveSpeed > 0.7f)
+        {
+            anim.SetBool("Pant", true);
+        }
+
+        if (moveSpeed < 0.05 && Vector3.Distance(agent.pathEndPosition, transform.position) < 0.05f)
+        {
+            if (shouldRelaxAfterMoving)
+            {
+                shouldRelaxAfterMoving = false;
+                ChangeState(State.RELAXING);
+            }
+            else
+            {
+                ChangeState(State.IDLE);
+            }
+        }
+
+        if (debugVisualisation)
+        {
+            for (int i = 0; i < path.corners.Length - 1; i++)
+            {
+                Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red);
+            }
+        }
+    }
+
+    private bool CanMove()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle")
+            || anim.GetCurrentAnimatorStateInfo(0).IsName("Motion")
+            || anim.GetCurrentAnimatorStateInfo(0).IsName("female_idle_pant")
+            || anim.GetCurrentAnimatorStateInfo(0).IsName("male_idle_pant"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    #endregion
+
+    #region NAVMESH
     public void ProcessNewPath(TouchInput.PlayerTouch _touchInfo)
     {
+        ChangeState(State.IDLE);
+
         Ray ray = Camera.main.ScreenPointToRay(_touchInfo.touchEnd);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
         {
-            destination = hit.point;
+            MoveTo(hit.point);
         }
+    }
 
+    private void MoveTo(Vector3 pos)
+    {
+        destination = pos;
         StartCoroutine(GetPath());
     }
 
@@ -100,13 +237,10 @@ public class Employee : MonoBehaviour
 
             yield return null;
         }
-
+        ChangeState(State.MOVING);
+        agent.destination = destination;
         agent.SetPath(path);
     }
 
-    //private IEnumerator Panting()
-    //{
-    //    yield return new WaitForSeconds(2.0f);
-    //    anim.SetBool("Pant", false);
-    //}
+    #endregion
 }
