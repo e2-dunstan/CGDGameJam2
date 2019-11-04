837567d63d33878091f58bc41cb5db36dfba293d
Ellie <ed181197@gmail.com>
2019-11-04 15:22:46 +0000

    Added a scroll texture script to be added to a line renderer

---
+++ Assets/VFX/ScrollTexture.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTexture : MonoBehaviour
+{
+    private Material mat;
+    private float offset = 0;
+    private float scrollSpeed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<LineRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset += Time.deltaTime * scrollSpeed;
+        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
