742e59c26ae29668b53c20a758bc9b13d1582022
Lewis <lewis2.bond@live.uwe.ac.uk>
2019-11-05 20:18:01 +0000

    #12 Added error audio back in

--- Assets/Scripts/JobManager/JobManager.cs
+++ Assets/Scripts/JobManager/JobManager.cs
@@ -140,6 +140,7 @@ public class JobManager : MonoBehaviour
                                     int randomNumber = Random.Range(2, 3);
                                     Job.GenericInt genericInt = new Job.GenericInt(randomNumber);
                                     job.eventList.Add<Job.GenericInt>(Event.REQUIRE_NUMBER_OF_PEOPLE, genericInt, Color.red);
+                                    AudioManager.Instance.Play(AudioManager.SoundsType.MISC, (int)AudioManager.MiscSounds.ERROR, 0.1f);
                                     break;
                                 }
                             case 1:
@@ -147,6 +148,7 @@ public class JobManager : MonoBehaviour
                                     int randomNumber = Random.Range(1, 2);
                                     Job.GenericInt genericInt = new Job.GenericInt(randomNumber);
                                     job.eventList.Add<Job.GenericInt>(Event.REQUIRE_BLUE_PERSON, genericInt, Color.red);
+                                    AudioManager.Instance.Play(AudioManager.SoundsType.MISC, (int)AudioManager.MiscSounds.ERROR, 0.1f);
                                     break;
                                 }
                             case 2:
@@ -154,6 +156,7 @@ public class JobManager : MonoBehaviour
                                     int randomNumber = Random.Range(1, 2);
                                     Job.GenericInt genericInt = new Job.GenericInt(randomNumber);
                                     job.eventList.Add<Job.GenericInt>(Event.REQUIRE_PINK_PERSON, genericInt, Color.red);
+                                    AudioManager.Instance.Play(AudioManager.SoundsType.MISC, (int)AudioManager.MiscSounds.ERROR, 0.1f);
                                     break;
                                 }
                             case 3:
@@ -164,6 +167,7 @@ public class JobManager : MonoBehaviour
                                     Debug.Log(randomColour);
                                     job.eventList.Add<Job.GenericGameObject>(Event.REQUIRE_ITEM, genericObj, randomColour);
                                     tempObj.GetComponent<CollectableItem>().SetParticleColour(randomColour);
+                                    AudioManager.Instance.Play(AudioManager.SoundsType.MISC, (int)AudioManager.MiscSounds.ERROR, 0.1f);
                                     break;
                                 }
                         }
