28500c38af4f78aad7e7f762f2e5847b421426f4
Alex Allman <xalexxd@hotmail.co.uk>
2019-11-05 17:57:21 +0000

    Fixed JSON pathing issues with build

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -222,7 +222,6 @@ public class AudioManager : MonoBehaviour
         switch (type)
         {
             case SoundsType.TASK:
-                Debug.Log(sound);
                     return ref taskSounds[sound];
         }
 
